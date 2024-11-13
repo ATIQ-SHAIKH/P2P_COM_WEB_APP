04e6aad71261f73506fe69ca508330ab0c43f2b4
atiq-aib <atiq@aibuddha.in>
2024-11-13 21:27:24 +0530

    :adhesive_bandage: trying to get it work with vercel be

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     env: {
-        BACKEND_URL: "http://localhost:9999/api"
-        // BACKEND_URL: "http://memo-meet-hoor6scnx-atiq-builds.vercel.app"
+        BACKEND_URL: "https://memo-meet-be.vercel.app/api"
     }
 };
 
